f55847fffb2df7106f1b995a6c9ca15377e5627b
Bradley <57405760+Bradley00Uni@users.noreply.github.com>
2022-03-07 22:01:33 +0000

    Add project files.

---
+++ WorkoutsAPI-master/ToyShopAPI/Models/ExerciseModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WorkoutAPI.Models
+{
+    public class ExerciseModel
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public int ActivityID { get; set; }
+
+        [ForeignKey("ActivityID")]
+        public ActivityModel? Activity { get; set; }
+
+        [Required]
+        public double? Duration { get; set; }
+    }
+}
---
+++ WorkoutsAPI-master/ToyShopAPI/Models/WorkoutModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WorkoutAPI.Models
+{
+    public class WorkoutModel
+    {
+        [Key]
+        [Required]
+        public int ID { get; set; }
+
+    }
+}
