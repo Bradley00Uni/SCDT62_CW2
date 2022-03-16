33225948afb7222b85cb24592c3f0b86d6b8b4c2
Bradley <57405760+Bradley00Uni@users.noreply.github.com>
2022-03-16 17:52:19 +0000

    Update workouts.js
    Workout Dates are now output in more user friendly format

--- WorkoutCompanion/components/workouts.js
+++ WorkoutCompanion/components/workouts.js
@@ -18,6 +18,60 @@ const Workouts = () => {
         .catch((error) => {console.log(error)})
     },)
 
+    const convertDate =(date) => {
+
+        var newDate = date.toString()
+        var year = newDate.substring(0,4)
+        var month = newDate.substring(5,7);
+        var day = newDate.substring(8,10);
+
+        switch (month) {
+            case "01":
+                month = "January";
+                break;
+            case "02":
+                month = "February";
+                break;
+            case "03":
+                month = "March";
+                break;
+            case "04":
+                month = "April";
+                break;
+            case "05":
+                month = "May";
+                break;
+            case "06":
+                month = "June";
+                break;
+            case "07":
+                month = "July";
+                break;
+            case "08":
+                month = "August";
+                break;
+            case "09":
+                month = "September";
+                break;
+            case "10":
+                month = "October";
+                break;
+            case "11":
+                month = "November";
+                break;
+            case "12":
+                month = "December";
+                break;
+        }
+
+        newDate = day + " " + month + " " + year
+
+        return newDate
+    }
+
+
+
+
    if(loading){
        return (
         <View style={styles.container}>
@@ -27,15 +81,16 @@ const Workouts = () => {
    }
    else{
        let works = workouts.map((val, key) => {
-           
+
             let totalDuration = 0;
             val.exercises.forEach(element => {
                 totalDuration = totalDuration + element.duration
             });
+            let date = convertDate(val.workout.workoutCreated) 
            return(
                <View key={key}>
                    <Card key={key} style={styles.item} containerStyle={{backgroundColor: '#F1E0B0'}}>
-                       <Card.Title h5>{val.workout.workoutCreated}</Card.Title>
+                       <Card.Title h5>{date}</Card.Title>
                        <Card.Divider />
                        <Text>Number of Activities: {val.exercises.length}</Text>
                        <Text>Total Duration: {totalDuration}</Text>
@@ -70,6 +125,9 @@ const styles = StyleSheet.create({
         backgroundColor: 'green',
         flexDirection: 'row',
         borderRadius: 80
+    },
+    scrolling: {
+        width: Dimensions.get('window').width,
     },
 })
 
